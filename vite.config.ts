5664fcd2e77298d6c83baa1daad467d061de99cd
ifan3735 <ifanmaina@gmail.com>
2024-07-23 10:19:03 +0300

    26 commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure this is 'dist'
+    outDir: 'build', // Ensure this is 'dist'
     rollupOptions: {
       output: {
         manualChunks: {
